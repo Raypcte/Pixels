935781b267f6bf83a1f2c9ac33d6c33f12605401
Raypcte <rayanevalak@gmail.com>
2022-05-29 01:31:24 -0300

    correcao lint

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
-//2 - Adicione à página uma paleta contendo quatro cores distintas.
+// 2 - Adicione à página uma paleta contendo quatro cores distintas.
 const paletaDeCores = document.getElementById('color-palette');
 const quadroDePixel = document.getElementById('pixel-board');
 
 function criarQuadrado(quantidade) {
   for (let index = 0; index < quantidade; index += 1) {
-    //Cria uma div
+    // Cria uma div
     const div = document.createElement('div');
     // adiciona uma classe na div
     div.classList.add('color');
-    div.classList.add('quadrado' + index);
+    div.classList.add(`quadrado${index}`);
     paletaDeCores.appendChild(div);
   }
 }
@@ -16,9 +16,9 @@ criarQuadrado(4);
 
 function criarTabela() {
   for (let index = 0; index < 5; index += 1) {
-    //Nesse for estou repetindo a logica das linhas.
+    // Nesse for estou repetindo a logica das linhas.
     const section = document.createElement('section');
-    for (let index = 0; index < 5; index += 1) {
+    for (let iLinha = 0; iLinha < 5; iLinha += 1) {
       // Nesse for estou criando a linha da tabela.
       const div = document.createElement('div');
       div.classList.add('pixel');
