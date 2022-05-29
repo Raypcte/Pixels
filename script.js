3029a63112db213a6f06110129a9579f67d01db0
Raypcte <rayanevalak@gmail.com>
2022-05-29 01:08:41 -0300

    pt 3 e 4

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 //2 - Adicione à página uma paleta contendo quatro cores distintas.
 const paletaDeCores = document.getElementById('color-palette');
+const quadroDePixel = document.getElementById('pixel-board');
 
 function criarQuadrado(quantidade) {
   for (let index = 0; index < quantidade; index += 1) {
@@ -7,8 +8,23 @@ function criarQuadrado(quantidade) {
     const div = document.createElement('div');
     // adiciona uma classe na div
     div.classList.add('color');
-    div.classList.add('quadrado' + index)
+    div.classList.add('quadrado' + index);
     paletaDeCores.appendChild(div);
   }
 }
 criarQuadrado(4);
+
+function criarTabela() {
+  for (let index = 0; index < 5; index += 1) {
+    //Nesse for estou repetindo a logica das linhas.
+    const section = document.createElement('section');
+    for (let index = 0; index < 5; index += 1) {
+      // Nesse for estou criando a linha da tabela.
+      const div = document.createElement('div');
+      div.classList.add('pixel');
+      section.appendChild(div);
+      quadroDePixel.appendChild(section);
+    }
+  }
+}
+criarTabela();
