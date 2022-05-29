c288ab8ec9f48447522ed65ba6df6a158eb41738
Raypcte <rayanevalak@gmail.com>
2022-05-29 02:47:48 -0300

    pt 7

--- script.js
+++ script.js
@@ -2,10 +2,21 @@
 const paletaDeCores = document.getElementById('color-palette');
 const quadroDePixel = document.getElementById('pixel-board');
 
+function clickDaPaleta(event) {
+  const corDiv = event.target;
+  const todasAsCores = document.querySelectorAll('.color');
+  for (let index = 0; index < todasAsCores.length; index += 1) {
+    console.log(index);
+    todasAsCores[index].classList.remove('selected');
+  }
+  corDiv.classList.add('selected');
+}
+
 function criarQuadrado(quantidade) {
   for (let index = 0; index < quantidade; index += 1) {
     // Cria uma div
     const div = document.createElement('div');
+    div.addEventListener('click', clickDaPaleta);
     // adiciona uma classe na div
     if (index === 0) {
       div.classList.add('selected');
